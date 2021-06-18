70e5f1a1c5707034418ad07372b9cafa6f815a7b
Thanh Hải Hoàng <haiht27112001@gmail.com>
2021-06-19 01:59:28 +0700

    Added new files

---
+++ index.php
@@ -0,0 +1,311 @@
+<!DOCTYPE HTML>
+<!--[if IE 6]>
+<html id="ie6" class="ie ie6 lt-ie9">
+<![endif]-->
+<!--[if IE 7]>
+<html id="ie7" class="ie ie7 lt-ie9">
+<![endif]-->
+<!--[if IE 8]>
+<html id="ie8" class="ie ie8 lt-ie9">
+<![endif]-->
+<!--[if gte IE 9]>
+<html class="ie ie9">
+<![endif]-->
+<!--[if !(IE)]><!-->
+<html>
+<!--<![endif]-->
+<head>
+  <meta http-equiv='Pragma' content='no-cache' />
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+  <meta name="description" content="张戈和亮亮的爱情故事：不经意相识, 或许是缘分，或许是注定；以后不管有怎么样，我都会在；就算全世界都都抛弃你，记得还有我；再累都不用害怕，因为你有我。记得在你最需要的时候想起我；我会一直陪着你。">
+  <meta name="keywords" content="IT男的浪漫，求婚表白网页" />
+  <link rel="shortcut icon" href="//res.zgboke.com/love/img/favicon.ico" />
+  <link rel="bookmark" href="//res.zgboke.com/love/img/favicon.ico" type="image/x-icon" />
+  <title>张戈❤亮亮-爱的故事-执子之手，与子偕老 | 张戈博客</title>
+  <link rel="stylesheet" href="css/all.min.css">
+  <!--百度统计代码-->
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "//hm.baidu.com/hm.js?f2826885e8c15bc6de5504516e18a28f";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
+</head>
+<body>
+  <div class="flowtime">
+    <div class="ft-section section-1" data-id="section-1">
+      <div id="/section-1/page-1" class="ft-page page-1" data-id="page-1">
+        <p class="text1">张戈❤亮亮 - 执子之手与子偕老.</p>
+        <p class="text2">按键盘 "↓" 开始倾听我们的故事...</p>
+      </div>
+      <div id="/section-1/page-2" class="ft-page page-2" data-id="page-2">
+        <p class="top-text">2010年5月1日之前的张戈一直是一个人生活，享受着孤独，也憧憬着爱情。</p>
+        <img src='img/iali63.jpg'  alt="2010年5月1日之前的张戈一直是一个人生活，享受着孤独，也憧憬着爱情。" />
+      </div>
+      <div id="/section-1/page-3" class="ft-page page-3 left-img" data-id="page-3">
+        <h2 class="text">一个人的走廊...</h2>
+        <img src='img/iali35.jpg'  alt="一个人的走廊..."/>
+      </div>
+      <div id="/section-1/page-4" class="ft-page page-4 full-img" data-id="page-4">
+        <h2 class="center-text">一个人的发呆...</h2>
+        <img src='img/iali6.jpg'  alt="一个人的发呆..." />
+      </div>
+    </div>
+    <div class="ft-section section-2" data-id="section-2">
+      <div id="/section-2/page-1" class="ft-page page-5 full-img" data-id="page-1">
+        <h2 class="center-text">一个人的出门...</h2>
+        <img src='img/iali19.jpg'  alt="一个人的出门..." />
+      </div>
+      <div id="/section-2/page-2" class="ft-page page-6" data-id="page-2">
+        <h2 class="center-text">一个人的做梦...</h2>
+        <img src='img/iali29.jpg' alt="一个人的做梦..."/>
+      </div>
+      <div id="/section-2/page-3" class="ft-page page-7" data-id="page-3">
+        <h3>但他依然乐观，微笑着，等待着</h3>
+        <img src='img/iali60.jpg'  alt="但他依然乐观，微笑着，等待着"/>
+      </div>
+      <div id="/section-2/page-4" class="ft-page page-8 full-img" data-id="page-4">
+        <h3>生活难免有风风雨雨</h3>
+        <img src='img/iali51.jpg' alt="生活难免有风风雨雨" />
+      </div>
+      <div id="/section-2/page-5" class="ft-page page-9" data-id="page-5">
+        <h3>他总是能够轻松的应对</h3>
+        <img src='img/iali5.jpg' alt="他总是能够轻松的应对" />
+      </div>
+      <div id="/section-2/page-6" class="ft-page page-10" data-id="page-6">
+        <h3>并且面带阳光、自信的笑容</h3>
+        <img src='img/iali22.jpg' alt="并且面带阳光、自信的笑容" />
+      </div>
+    </div>
+    <div class="ft-section section-3" data-id="section-3">
+      <div id="/section-3/page-1" class="ft-page page-11 full-img" data-id="page-1">
+        <h3>生活也不会总是一帆风顺...</h3>
+        <img src='img/iali24.jpg' alt="生活也不会总是一帆风顺..." />
+      </div>
+      <div id="/section-3/page-2" class="ft-page page-12" data-id="page-2">
+        <h3>但他每次都能勇敢的面对<br />随时准备接受生活的挑战</h3>
+        <img src='img/iali25.jpg' alt="但他每次都能勇敢的面对" />
+      </div>
+      <div id="/section-3/page-3" class="ft-page page-13" data-id="page-3">
+        <img src='img/iali64.jpg' alt='可是张戈的爱情又在哪里呢？' />
+        <h3>可是张戈的爱情又在哪里呢？</h3>
+      </div>
+      <div id="/section-3/page-4" class="ft-page page-14 left-img" data-id="page-4">
+        <h3>在镜子里面吗？他不敢相信</h3>
+        <img src='img/iali46.jpg' alt="在镜子里面吗？他不敢相信" />
+      </div>
+      <div id="/section-3/page-5" class="ft-page page-15 left-img" data-id="page-5">
+        <h3>他去问大树，我的爱情在哪里？<br />大树告诉他，也许就在不远的地方</h3>
+        <img src='img/iali45.jpg' alt="他去问大树，我的爱情在哪里？" />
+      </div>
+      <div id="/section-3/page-6" class="ft-page page-16 left-img" data-id="page-6">
+        <h3>于是，张戈一个人继续向前走<br />走在茫茫的雪地上</h3>
+        <img src='img/iali66.jpg' alt="于是，张戈一个人继续向前走" />
+      </div>
+      <div id="/section-3/page-7" class="ft-page page-17 top-text" data-id="page-7">
+        <h3>直到有一天张戈与亮亮相遇了</h3>
+        <img src='img/iali16.jpg' alt="直到有一天张戈与亮亮相遇了" />
+      </div>
+    </div>
+    <div class="ft-section section-4" data-id="section-4">
+      <div id="/section-4/page-1" class="ft-page page-18 full-img" data-id="page-1">
+        <p class="text">张戈喜欢亮亮，因为亮亮的出现，张戈脸上有了更加灿烂的笑容</p>
+        <img src='img/iali11.jpg' alt="张戈喜欢亮亮，因为亮亮的出现，张戈脸上有了更加灿烂的笑容" />
+      </div>
+      <div id="/section-4/page-2" class="ft-page page-19" data-id="page-2">
+        <h3>可是亮亮会喜欢张戈吗？</h3>
+        <img src="img/iali75.jpg" alt="可是亮亮会喜欢张戈吗？" />
+      </div>
+      <div id="/section-4/page-3" class="ft-page page-20" data-id="page-3">
+        <img src='img/iali59.png' alt="终于有一天，张戈鼓起了勇气" />
+        <h3>终于有一天，张戈鼓起了勇气</h3>
+        <img src='img/iali9.jpg' />
+      </div>
+      <div id="/section-4/page-4" class="ft-page page-21" data-id="page-4">
+        <h3>张戈好高兴</h3>
+        <img src='img/iali59.png' alt="张戈好高兴" />
+      </div>
+      <div id="/section-4/page-5" class="ft-page page-22 left-img" data-id="page-5">
+        <h3>每天张戈都会去找亮亮</h3>
+        <img src='img/iali4.jpg' alt="每天张戈都会去找亮亮" />
+      </div>
+      <div id="/section-4/page-6" class="ft-page page-23 left-img" data-id="page-6">
+        <h3>然后两个人一起出去玩</h3>
+        <img src='img/iali44.jpg' alt="然后两个人一起出去玩" />
+      </div>
+      <div id="/section-4/page-7" class="ft-page page-24 full-img" data-id="page-7">  
+        <h3>晚上张戈会把亮亮送回家</h3>
+        <img src='img/iali32.jpg' alt="晚上张戈会把亮亮送回家" />
+      </div>
+    </div>
+    <div class="ft-section section-5" data-id="section-5">
+      <div id="/section-5/page-1" class="ft-page page-25 left-img" data-id="page-1">
+        <h3>直到很晚<br />张戈才会一个人回家</h3>
+        <img src='img/iali37.jpg' alt="直到很晚" />
+      </div>
+      <div id="/section-5/page-2" class="ft-page page-26 top-text" data-id="page-2">
+        <h3>然后高兴地进入梦乡，希望梦到亮亮...</h3>
+        <img src='img/iali10.gif' alt="然后高兴地进入梦乡，希望梦到亮亮..." />
+      </div>
+      <div id="/section-5/page-3" class="ft-page page-27" data-id="page-3">
+        <h3>张戈很快就成为了亮亮的逛街助手</h3>
+        <img src='img/iali67.jpg' alt="张戈很快就成为了亮亮的逛街助手" />
+      </div>
+      <div id="/section-5/page-4" class="ft-page page-28" data-id="page-4">
+        <h3>张戈和亮亮一起去了好多地方玩</h3>
+        <img src='img/iali40.jpg' alt="张戈和亮亮一起去了好多地方玩" />
+      </div>
+      <div id="/section-5/page-5" class="ft-page page-29" data-id="page-5">
+        <h3>张戈也和亮亮一样成为了一个吃货</h3>
+        <img src='img/iali12.jpg' alt="张戈也和亮亮一样成为了一个吃货" />
+      </div>
+      <div id="/section-5/page-6" class="ft-page page-30" data-id="page-6">
+        <h3>后来，他们搬到了一起</h3>
+        <img src='img/iali76.gif' alt="后来，他们搬到了一起" />
+      </div>
+      <div id="/section-5/page-7" class="ft-page page-31" data-id="page-7">
+        <h3>于是张戈开始学习新的技能</h3>
+        <img src='img/iali77.jpg' alt="于是张戈开始学习新的技能" />
+      </div>
+    </div>
+    <div class="ft-section section-6" data-id="section-6">
+      <div id="/section-6/page-1" class="ft-page page-32" data-id="page-1">
+        <h3>向更多的美味发起进攻...</h3>
+        <img src='img/iali62.jpg' alt="向更多的美味发起进攻..." />
+      </div>
+      <div id="/section-6/page-2" class="ft-page page-33 top-text" data-id="page-2">
+        <h3>每天早上，他们吃着自己做的美食</h3>
+        <img src='img/iali3.jpg' alt="每天早上，他们吃着自己做的美食" />
+      </div>
+      <div id="/section-6/page-3" class="ft-page page-34 left-img" data-id="page-3">
+        <h3>然后在同一个站台，高高兴兴的一起上班</h3>
+      </div>
+      <div id="/section-6/page-4" class="ft-page page-35" data-id="page-4">
+        <h3>张戈感觉自己好幸福。<br />因为自己有了家<br />那个有亮亮在的地方</h3>
+        <img src='img/iali8.gif' alt="张戈感觉自己好幸福。" />
+      </div>
+      <div id="/section-6/page-5" class="ft-page page-36 full-img" data-id="page-5">
+        <h3>他们偶尔也会吵架</h3>
+        <img src='img/iali31.jpg' alt="他们偶尔也会吵架" />
+      </div>
+      <div id="/section-6/page-6" class="ft-page page-37 left-img" data-id="page-6">
+        <h3>张戈不想这样</h3>
+        <img src='img/iali49.jpg' alt="张戈不想这样" />
+      </div>
+      <div id="/section-6/page-7" class="ft-page page-38 bottom-text" data-id="page-7">
+        <img src='img/iali21.jpg' alt="一定是我有什么做的不对，张戈在想..." />
+        <h3>一定是我有什么做的不对，张戈在想...</h3>
+      </div>
+    </div>
+    <div class="ft-section section-7" data-id="section-7">
+      <div id="/section-7/page-1" class="ft-page page-39 left-img" data-id="page-1">
+        <h3>如果没有亮亮在身边，窗外就没有风景</h3>
+        <img src='img/iali2.jpg' alt="如果没有亮亮在身边，窗外就没有风景" />
+      </div>
+      <div id="/section-7/page-2" class="ft-page page-40" data-id="page-2">
+        <h3>如果没有亮亮在身后</h3>
+        <img src='img/iali14.jpg' alt="{text45_}" />
+      </div>
+      <div id="/section-7/page-3" class="ft-page page-41" data-id="page-3">
+        <img src='img/iali30_1.jpg' class='img1' alt="张戈又怎会飞的更高更远" />
+        <img src='img/iali30_2.jpg' class='img2' alt="张戈又怎会飞的更高更远2" />
+        <h3>张戈又怎会飞的更高更远</h3>
+      </div>
+      <div id="/section-7/page-4" class="ft-page page-42" data-id="page-4">
+        <h3>张戈不想这样。他要为亮亮改变自己<br />亮亮说她喜欢狮子座的人，于是张戈许下愿望</h3>
+        <img src='img/iali69.jpg' alt="张戈不想这样。他要为亮亮改变自己" />
+      </div>
+      <div id="/section-7/page-5" class="ft-page page-43" data-id="page-5">
+        <img src='img/iali42.jpg' alt="我愿为你变成狮子座..." />
+        <h3>我愿为你变成狮子座...</h3>
+      </div>
+      <div id="/section-7/page-6" class="ft-page page-44 left-img" data-id="page-6">
+        <h3>爱情就像花草一样<br />需要用包容来浇灌</h3>
+        <img src='img/iali0.jpg' alt="爱情就像花草一样" />
+      </div>
+      <div id="/section-7/page-7" class="ft-page page-45 left-img" data-id="page-7">
+        <h3>需要选一个重要的日子<br />去完成爱的承诺...</h3>
+        <img src='img/iali18.jpg' alt="需要选一个重要的日子" />
+      </div>
+    </div>
+    <div class="ft-section section-8" data-id="section-8">
+      <div id="/section-8/page-1" class="ft-page page-46" data-id="page-1">
+        <img src='img/iali57.gif' alt="带着独有的浪漫的浪漫" />
+        <h3>
+          <span>带着IT男独有的浪漫</span>
+          <span>张戈好开心</span>
+          <span>因为他想象着亮亮看到这个后</span>
+          <span>开心雀跃的样子</span>
+        </h3>
+      </div>
+      <div id="/section-8/page-2" class="ft-page page-47 top-text" data-id="page-2">
+        <img src='img/iali58.jpg' alt="想想后面还有一辈子" />
+        <h3>
+          <span>想想后面还有一辈子</span>
+          <span>张戈和亮亮都好开心</span>
+          <span>因为可以一起</span>
+          <span>去全世界好多地方玩</span>
+        </h3>
+      </div>
+      <div id="/section-8/page-3" class="ft-page page-48 left-img" data-id="page-3">
+        <p>张戈也会继续努力<br />为了他和亮亮的梦之城堡</p>
+        <img src='img/iali65.jpg' alt="张戈也会继续努力" />
+      </div>
+      <div id="/section-8/page-4" class="ft-page page-49 full-img" data-id="page-4">
+        <p class='text'>张戈很幸福，因为他找到了亮亮。他相信后面的每天都会是快乐的<br />简简单单，享受着与亮亮在一起的每一个刻</p>
+        <img src='img/iali71.jpg' alt="张戈很幸福，因为他找到了亮亮。他相信后面的每天都会是快乐的" />
+      </div>
+      <div id="/section-8/page-5" class="ft-page page-50" data-id="page-5">
+        <p class='text'>这是属于张戈和亮亮的幸福生活。</p>
+        <img src='img/iali50_1.jpg' class='img1' alt="这是属于张戈和亮亮的幸福生活。" />
+        <img src='img/iali50_2.jpg' class='img2' alt="这是属于张戈和亮亮的幸福生活。2" />
+      </div>
+      <div id="/section-8/page-6" class="ft-page page-51" data-id="page-6">
+        <p>
+          亮亮：永远有多远？<br />
+          张戈：比时间多一秒就是永远，我会永远爱你<br />
+          亮亮：世界有多大？<br />
+          张戈：你走到哪里，世界就有多大
+        </p>
+      </div>
+    </div>
+    <div class="ft-section section-9" data-id="section-9">
+      <div id="/section-9/page-1" class="ft-page page-52 full-img" data-id="page-1">
+        <h3>愿得一人心，白首不相离</h3>
+        <img src='img/iali20.jpg' alt="愿得一人心，白首不相离" />
+      </div>
+      <div id="/section-9/page-2" class="ft-page page-53" data-id="page-2">
+        <img src='img/iali68.jpg' alt="亮亮，嫁给张戈吧！" />
+        <h3>亮亮，嫁给张戈吧！</h3>
+      </div>
+      <div id="/section-9/page-3" class="ft-page page-54 center-img" data-id="page-3" >
+        <div class="center-img">I love you</div>
+      </div>
+      <div id="/section-9/page-4" class="ft-page page-55 right-img" data-id="page-4">
+        <img src='img/14915.jpg' alt="张戈和亮亮亮亮的故事会一直继续下去。" />
+        <p class="text">
+          张戈和亮亮的故事会一直继续下去。<br />
+          无论精彩、平淡都会是他们喜欢的。<br />
+          <br />
+          <br />
+          <span> -- 按“Esc"键有惊喜(可能需要切到IE兼容模式...)</span>
+          <br />
+          <br />     
+          <span>喜欢吗？那就<a target="_blank" href="love/index.html">点此制作一个</a>，给你亲爱的一个惊喜吧！</span>
+        </p>        
+      </div>
+    </div>
+  </div>
+<div class="nojavascript">资源载入中...若失败请刷新页面.</div> 
+<audio id="bgmMusic" src="//zhang.ge/love/mp3/xgbb.mp3" preload="auto" type="audio/mp3" autoplay loop></audio> 
+  <script src="js/jquery.min.js"></script>
+  <script src="js/all.min.js"></script>
+  <script src="js/love.min.js"></script>
+<div class="mPower"><span id="on" title="点击暂停"></span><span id="off" title="点击播放"></span></div>
+<script>window._bd_share_config={"common":{"bdSnsKey":{},"bdText":"不经意相识, 或许是缘分，或许是注定；以后不管有怎么样，我都会在；就算全世界都都抛弃你，记得还有我；再累都不用害怕，因为你有我。记得在你最需要的时候想起我；我会一直陪着你。","bdMini":"2","bdMiniList":["tsina","qzone","weixin","sqq","tqq","qq","tqf","copy","bdysc","baidu","hi","share189","renren","fx","mail","mshare"],"bdPic":"//res.zgboke.com/love/img/lovers.jpg","bdStyle":"0","bdSize":"16"},"slide":{"type":"slide","bdImg":"1","bdPos":"right","bdTop":"200"},"image":{"viewList":["tsina","qzone","weixin","tqq"],"viewText":"唯美的爱，期待您的分享！","viewSize":"24"},"selectShare":{"bdContainerClass":null,"bdSelectMiniList":["tsina","qzone","weixin","tqq"]}};with(document)0[(getElementsByTagName('head')[0]||body).appendChild(createElement('script')).src='//o1r3y2z3i.qnssl.com/static/api/js/share.js?v=89860593.js?cdnversion='+~(-new Date()/36e5)];</script>     
+</body>
+</html>
